674dd4008b093ff2a3d4fe9b1e58e0ace96fcc14
Tomas Vergara <tominacho@hotmail.com>
2025-11-22 12:15:21 -0300

    fix: se corrigio texto principal

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         <div className="relative z-10 flex w-full max-w-3xl flex-col items-center justify-center gap-8 px-16 py-32 text-center">
           <div className="flex flex-col items-center gap-6">
             <h1 className="max-w-2xl text-5xl font-bold leading-tight tracking-tight text-white">
-              Bienvenido a Tukancha
+              Bienvenido a TuKancha
             </h1>
             <p className="max-w-xl text-xl leading-8 text-zinc-200">
               La forma mas facil de reservar tu cancha. <br />
